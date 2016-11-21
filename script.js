739285df61df33984aaf86b927bc407fe1cdf857
Yaniv Izaki <Yaniv Izaki>
2016-11-21 15:58:26 +0200

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var WeatherChatApp = function (){
+var $city = $('#city');
+var $cityWeather = $('#city-weather');
 
-var $city = $('#city'); 
-var temp = -100;
+//var WeatherChatApp = function (){
 
 var fetch = function (city) {
 	var _url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=47fe5baf1d54cdcbbb5ecc689871294a';
@@ -11,19 +11,44 @@ var fetch = function (city) {
   dataType: "json",
   success: function(data) {
     console.log(data);
-    temp = Math.round(data.main.temp - 273);
-    var cel = temp + " Celsius";
-    $('#city-weather').append("The Temp in "+city+" is "+cel);
+    postTemp(data,city);
   },
   error: function(jqXHR, textStatus, errorThrown) {
     console.log(textStatus);
   }
 }); 
-}
 };
+//};
 
-var app = WeatherChatApp();
+//var app = WeatherChatApp();
 
+var postTemp = function (data,city) {
+    $cityWeather.empty();
+    var temp = Math.round(data.main.temp - 273);
+    var cel = temp + " Celsius";
+    $cityWeather.append("The Temp in "+city+" is "+cel);
+    addComment(comment);
+}
+
+var addComment = function () {
+  
+}
+
+// var listBook = function (data) {
+//   if(totalItems){
+//     var newBook = {
+//     bookName: ,
+//     time: ,
+//     desc: ,
+//     author: ,
+//     image: 
+//   }
+//   var source = $('#book-template').html();
+//   var template = Handlebars.compile(source);
+//   var newHTML = template(newBook);
+//   $('#info-book').append(newHTML);
+// }
+// };
 
 $('.btn-primary').on('click',function(e){
   e.preventDefault();
